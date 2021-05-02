102bb14783305cb26a044a2481554e155f1c31c9
amitwaghmaretorinit <amit@torinit.ca>
2021-05-02 17:20:49 +0530

    minor changes

--- src/pages/AgeCalculator/helper.js
+++ src/pages/AgeCalculator/helper.js
@@ -1,15 +1,18 @@
 export const findAge = (date) => {
     const today = new Date();
     const birthDate = date;
+    if (today && birthDate) {
+        const yearsDifference = today.getFullYear() - birthDate.getFullYear();
 
-    const yearsDifference = today.getFullYear() - birthDate.getFullYear();
+        if (
+            today.getMonth() < birthDate.getMonth() ||
+            (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
+        ) {
+            return yearsDifference - 1;
+        }
 
-    if (
-        today.getMonth() < birthDate.getMonth() ||
-        (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
-    ) {
-        return yearsDifference - 1;
+        return yearsDifference;
     }
+    return ""
 
-    return yearsDifference;
 }
--- src/pages/AgeCalculator/index.jsx
+++ src/pages/AgeCalculator/index.jsx
@@ -1,9 +1,11 @@
-import { Button, Paper } from '@material-ui/core'
+import { Button, Paper, Typography } from '@material-ui/core'
 import React, { useState } from 'react'
 import LayoutContainer from '../../components/LayoutContainer'
+import { findAge } from './helper'
 
 function AgeCalculator(props) {
     const { callBack = () => { } } = props
+    const [selectedDate, setSelectedDate] = useState(null)
     const [res, setRes] = useState('')
     const [error, setError] = useState('')
 
@@ -14,20 +16,24 @@ function AgeCalculator(props) {
             setError('Enter Past Date')
             return
         }
+        setSelectedDate(selectedDate)
         setError('')
+
     }
 
     const OnCalculateHandler = () => {
         callBack()
+        const age = findAge(selectedDate)
+        setRes(age)
     }
 
     return (
         <LayoutContainer>
             <Paper elevation={3} style={{ height: 500, width: 550, display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
                 <input onChange={onDateChangeHandler} type={'date'} placeholder={'Enter you birthdate'} />
-                <Button disabled={!!error} onClick={OnCalculateHandler} variant={'contained'} color={'primary'}>Calculate</Button>
+                 <Button disabled={!!error} onClick={OnCalculateHandler} variant={'contained'} color={'primary'}>Calculate</Button>
                 <span id='res'>{res}</span>
-                <span id='error'>{error}</span>
+                <Typography id='error'>{error}</Typography>
             </Paper>
         </LayoutContainer>
     )
